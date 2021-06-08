499142423f2882db09192ac73f4ee87a049f1ba4
jiaoruogu <ruogu.jiao@outlook.com>
2021-06-08 14:31:30 +0800

    change bubbleSort

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -5,6 +5,7 @@ const list = generateRandomArr(n, n)
 
 const list2 = [5,4,3,2,1]
 
+// 循环不变量 [0,i]
 function sort1 (arr) {
   for(let i=0;i<arr.length;i++) {
     for(let j = 0; j< arr.length - i -1; j++) {
@@ -13,10 +14,11 @@ function sort1 (arr) {
   }
 }
 
+// 循环不变量(i,n]
 function sort2 (arr) {
-  for(let i = arr.length - 1; i >= 0; i--) {
-    for(let j = i; j>=0; j--) {
-      
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = arr.length - 1; j>i; j--) {
+      if(arr[j] < arr[j - 1]) swap(arr, j, j - 1)
     }
   }
 }
@@ -29,8 +31,10 @@ function swap(arr, i, j) {
 
 
 const startTime = new Date().getTime()
-sort1(list)
+sort2(list2)
 const endTime = new Date().getTime()
 
-console.log(isSorted(list));
+console.log(isSorted(list2));
+console.log(list2);
+
 console.log((endTime-startTime) / 1000);
