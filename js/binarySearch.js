77e0c975a21da426ee13ff45fee823c3cd0a29af
jrg <1758043627@qq.com>
2022-02-22 13:56:28 +0800

    二分法查找数组指定元素

---
+++ js/binarySearch.js
@@ -0,0 +1,64 @@
+/**
+ * 关于二查找的算法
+ */
+
+const list = []
+for (let i = 0; i < 10000000; i++) {
+    list.push(i)
+}
+
+
+// 递归实现二分查找
+function binarySearch(arr, target, start, end) {
+    start = start ?? 0
+    end = end ?? arr.length - 1
+    let mid = Math.floor((start + end) / 2)
+
+    if (start <= end) {
+        if (target < arr[mid]) {
+            end = mid - 1
+            return binarySearch(arr, target, start, end)
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+            return binarySearch(arr, target, start, end);
+        } else {
+            return mid
+        }
+    } else {
+        return -1
+    }
+}
+
+
+
+// 循环实现二分查找
+function binarySearch2(arr, target, start, end) {
+    start = start ?? 0
+    end = end ?? arr.length - 1
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        if (target < arr[mid]) {
+            end = mid - 1;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
+
+
+
+
+const start2 = Date.now()
+console.log(binarySearch2(list,0))
+const end2 = Date.now()
+console.log(end2 - start2)
+
+
+
+
+
+
